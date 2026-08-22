ba0059b82b1700c3228afbb382c4afedf502b2ea
GDaksh07 <dakshgulati2007@gmail.com>
2026-08-21 22:28:14 -0400

    Test created for memory allocation and functions

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
-#include <iostream>
+#include <stdio.h>
+#include "student.h"
 
 int main() {
+    Database db;
+
+    // Initializes memory
+    printf("Creating database with a capacity of 2\n");
+    init_db(&db, 2);
+
+    // Adds students
+    printf("Adding students\n");
+    addStudent(&db, 101, 3.85f, "Alice Smith");
+    addStudent(&db, 102, 3.42f, "Bob Jones");
+    addStudent(&db, 103, 3.91f, "Charlie Brown");
+    addStudent(&db, 104, 2.95f, "Diana Prince");
+
+    displayAll(&db);
+
+    // Frees memory
+    free_db(&db);
+    printf("Memory freed successfully");
 
     return 0;
 }
